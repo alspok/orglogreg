0e72038fa1b193552c13557371801735af3c6ed9
alspok <alspok@gmail.com>
2019-01-15 00:19:16 +0200

    .

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
         </form>
         <h2>Registration</h2>
         <form method='post' action='registration.php'>
-            User name: <input type='text' name='reg-user'><br><br>
-            Email: <input type='text' name='reg-email'><br><br>
-            Password: <input type='password' name='reg-password'><br><br>
-            Organisation: <input type='selection' name='reg-org'><br><br>
+            User name: <input type='text' name='reg-user' value='kebabas'><br><br>
+            Email: <input type='text' name='reg-email' value='kebabas@kebabas.lt'><br><br>
+            Password: <input type='password' name='reg-password' value='kebabas'><br><br>
+            Organisation: <input type='selection' name='reg-org' value='dar vienas kebabas'><br><br>
             <input type='submit' name='submit' value='Submit'>
         </form>
     </body>
