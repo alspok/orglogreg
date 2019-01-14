e5157bd260a8e953d828f1770ce1cef80a33b2dd
alspok <alspok@gmail.com>
2019-01-14 23:47:27 +0200

    .

--- registration.php
+++ registration.php
@@ -1,14 +1,29 @@
 <?php
 
+require('mysqlconn.php');
 $regData = $_POST;
+$tblName = 'tbl_reg';
 
-function registrationValidation($regData)
+if(regValidation($regData)){
+    insertDB($regData);
+}
+
+//Function block
+function regValidation($regData)
 {
-    if(isset($regData['reg-user'] && isset($regData['reg-email']) &&
-       isset($regData['reg-password']) && isset($regData['reg-org']) && isset($regData['submit'])){
-        
-       } 
-       else echo 'Please enter all fields.'
+    if(isset($regData['reg-user']) && isset($regData['reg-email']) && isset($regData['reg-password']) && isset($regData['reg-org']) && isset($regData['submit'])){
+        return TRUE;
+        } 
+        else echo 'Enter all fields please.';
+}
 
+function insertDB($regData)
+{
+    $conn = mySqliConnection();
+    $queryString = 'INSERT INTO  ' . $tblName . ' (user, email, password, org) VALUES (????)';
+    $stmt = $conn->prepare($queryString);
+    $stmt->bind_param('ssss', $regData['reg-user'], $regData['reg-email'], $regData['reg-password'], $regData['reg-org']);
+    $stmt->execute();
+    $stmt->close();
 }
 
