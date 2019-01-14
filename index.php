28388722dd8a85784773180959405c42d2e3080a
alspok <alspok@gmail.com>
2019-01-14 20:26:07 +0200

    first try

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <h2>Login</h2>
+        <form method='post' action='login.php'>
+            User name: <input type='text' name='log-user'><br><br>
+            Email: <input type='text' name='log-email'><br><br>
+            Password: <input type='password' name='log-password'><br><br>
+            <input type='submit' name='submit' value='Submit'><br><br>
+        </form>
+        <h2>Registration</h2>
+        <form method='post' action='registration.php'>
+            User name: <input type='text' name='reg-user'><br><br>
+            Email: <input type='text' name='reg-email'><br><br>
+            Password: <input type='password' name='reg-password'><br><br>
+            Organisation: <input type='selection' name='organisation'><br><br>
+            <input type='submit' name='submit' value='Submit'>
+        </form>
+    </body>
+</html>
